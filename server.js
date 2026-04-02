af1f3d0725136834bae60ea110c8c96fac3304c2
TranThinh112 <ttthinh2005@gmail.com>
2026-04-02 19:06:05 +0700

    login

--- server.js
+++ server.js
@@ -299,7 +299,10 @@ app.put("/repass/users/:username", (req, res) => {
     }
   );
 });
-
+app.use((req, res, next) => {
+  console.log("👉", req.method, req.url);
+  next();
+});
 
 //////////////////////////////////////////////////////////////////////////////////// TO_orders ///////////////////////////////////////////////////////////////////////
 //gửi dữ liệu lên server để tạo TO mới
